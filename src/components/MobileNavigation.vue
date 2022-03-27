fac2a1cc48d26403e3c91574e1f59d5b943c5b28
Laisvūnas <ajcfromroots@gmail.com>
2022-03-27 22:51:19 +0300

    idk

--- src/components/MobileNavigation.vue
+++ src/components/MobileNavigation.vue
@@ -19,7 +19,7 @@ const menuOpen = ref(false)
       </div>
     </div>
     <MenuIcon :menu-open="menuOpen" class="w-[35px] cursor-pointer hover:scale-110 transition-all duration-500 z-50" @click="menuOpen = !menuOpen" />
-    <div :class="menuOpen ? 'overflow-hidden fixed top-0 right-0 w-[30vw] h-screen bg-angry text-white text-center z-40 transition-all duration-[700ms]' : 'overflow-hidden fixed top-0 right-0 w-[30vw] h-0 bg-angry text-white text-center z-40 transition-all duration-300'">
+    <div :class="menuOpen ? 'overflow-hidden fixed top-0 right-0 w-[30vw] h-[100vh] bg-angry text-white text-center z-40 transition-all duration-[700ms]' : 'overflow-hidden fixed top-0 right-0 w-[30vw] h-0 bg-angry text-white text-center z-40 transition-all duration-300'">
       <RouterLink v-for="(link, i) in navLinks" :key="i" :to="link.path" class="block transition-all duration-300 opacity-30 hover:opacity-100 hover:scale-105 uppercase mt-8 first-of-type:mt-[120px]" @click="menuOpen = !menuOpen">
         {{ link.name }}
       </RouterLink>
