0039a0499ac8986b30e3547f69e43c79f6189023
Laisvūnas <ajcfromroots@gmail.com>
2022-03-28 19:52:24 +0300

    w

--- src/components/Lightbox.vue
+++ src/components/Lightbox.vue
@@ -16,14 +16,3 @@ export default {
   }
 }
 </script>
-
-<style>
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.3s ease-in-out;
-}
-.fade-enter,
-.fade-leave-to {
-  opacity: 0;
-}
-</style>
